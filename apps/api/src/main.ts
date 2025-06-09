f62a6d6d33ff457f921c606094cf65b2ec1a7820
Joshua Chisolm <jkchisolm22@gmail.com>
2025-06-09 16:46:09 -0700

    fix better-auth implementation

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,16 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import express from 'express';
+import { toNodeHandler } from 'better-auth/node';
+import { auth } from './utils/auth';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Express app for auth middleware is initialized below
   // Definitely NOT the best pattern but we move
-  const auth = express();
-  auth.all('/api/auth/*splat');
+  const authMiddleware = express();
+  authMiddleware.all('/api/auth/*splat', toNodeHandler(auth));
 
-  app.use(auth);
+  app.use(authMiddleware);
 
   await app.listen(process.env.PORT ?? 4000);
 }
